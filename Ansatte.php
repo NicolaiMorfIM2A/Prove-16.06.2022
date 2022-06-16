c27ba077abd8d4c33f8ff916ad0d2bd89cfd5b85
NicolaiMorfIM2A <92984039+NicolaiMorfIM2A@users.noreply.github.com>
2022-06-16 14:06:19 +0200

    fjernet urelevant kommentar

--- Ansatte.php
+++ Ansatte.php
@@ -27,7 +27,6 @@ $sql = "SELECT Navn, Mobil, Jobb, Epost, Stilling, Avdeling, Bilde FROM prove.an
 $result = $kobling->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
     while($row = $result->fetch_assoc()) {
         $navn = $row["Navn"];
         $mobil = $row["Mobil"];
